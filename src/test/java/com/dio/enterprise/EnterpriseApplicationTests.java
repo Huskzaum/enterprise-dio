e7200fcde54a5cf15c5b71701206f516f6154717
Huskzaum <huskzaum2@gmail.com>
2021-07-04 15:10:34 -0300

    initial commit

---
+++ src/test/java/com/dio/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
